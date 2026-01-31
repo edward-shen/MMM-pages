400100e761074473fa21304737869692adb615be
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2026-01-31 17:50:58 +0100

    chore: add changelog config and release script

---
+++ changelog.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  types: [
+    { type: 'feat', section: 'Added' },
+    { type: 'fix', section: 'Fixed' },
+    { type: 'perf', section: 'Performance Improvements' },
+    { type: 'docs', section: 'Documentation' },
+    { type: 'chore', section: 'Chores' },
+    { type: 'refactor', section: 'Code Refactoring' },
+    { type: 'test', section: 'Tests' },
+    { type: 'build', section: 'Build System' },
+    { type: 'ci', section: 'Continuous Integration' },
+  ],
+};
